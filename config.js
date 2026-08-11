d80ce7f4ee8847ea708d1ff71479305bcbc9c0ee
bubdullahuae5001-create <bu.bdullahuae5001@gmail.com>
2026-08-11 08:22:24 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   APP_NAME: 'مشوار',
-  SUPABASE_URL: 'https://wjpexefljlbgqaxpkdsx.supabase.co',
+  SUPABASE_URL: 'https://wjpexefljlbqgaxpkdsx.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_pWoGj3-MYVDQnL7I35XEMg_dVAETM7G'
 };
